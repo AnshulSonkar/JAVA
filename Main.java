463641ea86ba6dc3308529f6a3f928dc0da7333c
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-19 14:07:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,98 @@
+// public class firstkrep{
+
+//     public static void main(string[] args) {
+        
+//     }
+    
+// }
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         String str = "geeksforgeeks";
+//         char result = ' ';
+//         boolean found = false;
+
+//         for (int i = 0; i < str.length(); i++) {
+//             boolean isRepeating = false;
+//             for (int j = 0; j < str.length(); j++) {
+                
+//                 if (i != j && str.charAt(i) == str.charAt(j)) {
+//                     isRepeating = true;
+//                     break;
+//                 }
+//             }
+            
+//             if (!isRepeating) {
+//                 result = str.charAt(i);
+//                 found = true;
+//                 break;
+//             }
+//         }
+
+//         if (found) {
+//             System.out.println("First non-repeating character is: " + result);
+//         } else {
+//             System.out.println("No non-repeating character found.");
+//         }
+//     }
+// }
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         String str = "aabbccdee";
+//         char result = ' ';
+//         boolean found = false;
+
+//         for (int i = 0; i < str.length(); i++) {
+//             boolean isRepeating = false;
+//             for (int j = 0; j < str.length(); j++) {
+                
+//                 if (i != j && str.charAt(i) == str.charAt(j)) {
+//                     isRepeating = true;
+//                     break;
+//                 }
+//             }
+            
+            
+//             if (!isRepeating) {
+//                 result = str.charAt(i);
+//                 found = true;
+//                 break;
+//             }
+//         }
+
+//         if (found) {
+//             System.out.println("First non-repeating character is: " + result);
+//         } else {
+//             System.out.println("No non-repeating character found.");
+//         }
+//     }
+// }
+
+
+// class Main {
+//     public static void main(String[] args) {
+
+//         String name1 = "Aman";
+//         String name2 = "Akku";
+//         String name3 = name1 +" "+ name2;
+
+//         System.out.println(name3);
+//     }
+// }
+
+
+class Main {
+   public static void main(String[] args) {
+    boolean isSunUp = false;
+
+        if (isSunUp == true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+
+ }
+}
+
