f8be1fdfb8ba6222e1a11427010e65e9c538ba98
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-23 21:15:32 +0530

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -1,21 +1,42 @@
+// import java.util.Arrays;
+
+// public class Anagram {
+//     public static void main(String[] args) {
+//     String s1="silent";
+//     String s2="listen";
+
+//     char[] a=s1.toCharArray();
+//     char[] b=s2.toCharArray();
+
+//     Arrays.sort(a);
+//     Arrays.sort(b);
+
+//     if (Arrays.equals(a, b)) {
+//         System.out.println("The strings are anagrams.");
+//     } else {
+//         System.out.println("The strings are not anagrams.");
+//     }
+
+//    }
+// }
+
+
 import java.util.Arrays;
 
 public class Anagram {
     public static void main(String[] args) {
-    String s1="silent";
-    String s2="listen";
+        String s1 = "Listen";
+        String s2 = "Silent";
 
-    char[] a=s1.toCharArray();
-    char[] b=s2.toCharArray();
+        s1 = s1.toLowerCase().replace(" ", "");
+        s2 = s2.toLowerCase().replace(" ", "");
 
-    Arrays.sort(a);
-    Arrays.sort(b);
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
 
-    if (Arrays.equals(a, b)) {
-        System.out.println("The strings are anagrams.");
-    } else {
-        System.out.println("The strings are not anagrams.");
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        System.out.println(Arrays.equals(a, b) ? "Anagram" : "Not Anagram");
     }
-
-   }
 }
