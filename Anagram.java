c762095a7a233b974b016732aadd84eec6111eec
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-24 23:38:57 +0530

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -21,22 +21,51 @@
 // }
 
 
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// public class Anagram {
+//     public static void main(String[] args) {
+//         String s1 = "Listen";
+//         String s2 = "Silent";
+
+//         s1 = s1.toLowerCase().replace(" ", "");
+//         s2 = s2.toLowerCase().replace(" ", "");
+
+//         char[] a = s1.toCharArray();
+//         char[] b = s2.toCharArray();
+
+//         Arrays.sort(a);
+//         Arrays.sort(b);
+
+//         System.out.println(Arrays.equals(a, b) ? "Anagram" : "Not Anagram");
+//     }
+// }
+
 
 public class Anagram {
     public static void main(String[] args) {
-        String s1 = "Listen";
-        String s2 = "Silent";
+        String s1 = "listen";
+        String s2 = "silent";
 
-        s1 = s1.toLowerCase().replace(" ", "");
-        s2 = s2.toLowerCase().replace(" ", "");
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Anagram");
+            return;
+        }
 
-        char[] a = s1.toCharArray();
-        char[] b = s2.toCharArray();
+        int[] count = new int[26];
 
-        Arrays.sort(a);
-        Arrays.sort(b);
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
 
-        System.out.println(Arrays.equals(a, b) ? "Anagram" : "Not Anagram");
+        for (int x : count) {
+            if (x != 0) {
+                System.out.println("Not Anagram");
+                return;
+            }
+        }
+
+        System.out.println("Anagram");
     }
 }
