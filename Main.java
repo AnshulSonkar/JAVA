0d9efafb836e54ce3d39041b3ab910403817c604
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-26 00:19:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -184,30 +184,48 @@
 // }
 
 
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// public class Main {
+
+//    static int fibo(int n, int[] dp) {
+//         if (n <= 1) {
+//             return n;
+//         }
+
+//         if (dp[n] != -1) {
+//             return dp[n];
+//         }
+
+//         dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+//         return dp[n];
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+
+//         System.out.println(fibo(n, dp));
+//     }
+// }
+
+
 
 public class Main {
-
-   static int fibo(int n, int[] dp) {
-        if (n <= 1) {
-            return n;
-        }
-
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-
-        dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
-        return dp[n];
-    }
-
-    public static void main(String[] args) {
-        int n = 5;
+public static void main(String[] args) {
+        int n = 4;
 
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
 
-        System.out.println(fibo(n, dp));
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
     }
 }
 
