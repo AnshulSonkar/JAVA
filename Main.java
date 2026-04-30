cb3398a9be8bd1eaef32bedee4954fb5d16d4816
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-30 12:17:51 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -271,14 +271,45 @@
 
 
 
-public class Main {
-    static int recursioin(int a,int b){
-    int sum=a+b;
-   return sum;
-  }
-    public static void main(String[] args) {
+// public class Main {
+//     static int recursioin(int a,int b){
+//     int sum=a+b;
+//    return sum;
+//   }
+//     public static void main(String[] args) {
        
-        System.out.println(recursioin(2,3));
-  }
+//         System.out.println(recursioin(2,3));
+//   }
+// }
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matrix = new int[2][2];
+
+        System.out.println("Enter elements:");
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix is:");
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
     
