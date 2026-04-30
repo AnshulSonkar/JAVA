7987b09d1e303a61204434001907e8475bf6249d
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-30 12:21:38 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -285,29 +285,52 @@
 
 
 
+// import java.util.Scanner;
+
+// public class Main {
+//  public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int[][] matrix = new int[2][2];
+
+//         System.out.println("Enter elements:");
+
+//         for(int i = 0; i < 2; i++) {
+//             for(int j = 0; j < 2; j++) {
+//                 matrix[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         System.out.println("Matrix is:");
+
+//         for(int i = 0; i < 2; i++) {
+//             for(int j = 0; j < 2; j++) {
+//                 System.out.print(matrix[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
 import java.util.Scanner;
 
 public class Main {
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] matrix = new int[2][2];
+        int rows = 3;
+        int cols = 3;
+        int[][] matrix = new int[rows][cols];
 
         System.out.println("Enter elements:");
 
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
                 matrix[i][j] = sc.nextInt();
             }
-        }
-
-        System.out.println("Matrix is:");
-
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < 2; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
         }
     }
 }
