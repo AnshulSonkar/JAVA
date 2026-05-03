fbf719fb9387c11543946279afe6386c772d9e52
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-03 22:33:15 +0530

    Add files via upload

--- Recursion.java
+++ Recursion.java
@@ -429,27 +429,54 @@
 //     }
 // }
 
+// import java.util.Arrays;
+
+// public class Recursion {
+// public static void main(String[] args) {
+//         int[] arr = {5, 3, 2, 4, 1};
+
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             for (int j = 0; j < arr.length - i - 1; j++) {
+
+//                 if (arr[j] > arr[j + 1]) {
+//                     // swap
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
+
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
+
 import java.util.Arrays;
 
 public class Recursion {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = {5, 3, 2, 4, 1};
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
 
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
 
         System.out.println(Arrays.toString(arr));
     }
 }
+
 
 
 
