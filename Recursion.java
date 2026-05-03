8a972dfa2cdbabdfea38463606fa719f96d5e3bd
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-03 22:44:30 +0530

    Add files via upload

--- Recursion.java
+++ Recursion.java
@@ -478,29 +478,39 @@
 // }
 
 
+// import java.util.Arrays;
+// public class Recursion {
+
+// public static void main(String[] args) {
+//         int[] arr = {5, 3, 2, 4, 1};
+
+//         for (int i = 1; i < arr.length; i++) {
+//             int current = arr[i];
+//             int j = i - 1;
+
+//             while (j >= 0 && arr[j] > current) {
+//                 arr[j + 1] = arr[j];
+//                 j--;
+//             }
+
+//             arr[j + 1] = current;
+//         }
+
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
+
 import java.util.Arrays;
 public class Recursion {
-
 public static void main(String[] args) {
-        int[] arr = {5, 3, 2, 4, 1};
+        int[] arr = {9, 7, 5, 3, 1};
 
-        for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > current) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-
-            arr[j + 1] = current;
-        }
+        Arrays.sort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
 }
-
-
 
 
 
