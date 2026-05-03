9595a1311648108cb328c0d5a465e9a39bbf2d5c
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-03 23:21:00 +0530

    Add files via upload

--- Recursion.java
+++ Recursion.java
@@ -528,29 +528,47 @@
 // }
 
 
-import java.util.Arrays;
-public class Recursion {
- public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+// import java.util.Arrays;
+// public class Recursion {
+//  public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
 
-        boolean swapped;
+//         boolean swapped;
+
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             swapped = false;
+
+//             for (int j = 0; j < arr.length - i - 1; j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                     swapped = true;
+//                 }
+//             }
+
+//             if (!swapped) break;
+//         }
+
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
+
+public class Recursion {
+public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+
+        boolean sorted = true;
 
         for (int i = 0; i < arr.length - 1; i++) {
-            swapped = false;
-
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                break;
             }
-
-            if (!swapped) break;
         }
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println(sorted ? "Sorted" : "Not Sorted");
     }
 }
     
