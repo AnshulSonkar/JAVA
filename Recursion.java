507c8a4c1747153159fe988f56b73756f9f44549
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-04 23:46:01 +0530

    Add files via upload

--- Recursion.java
+++ Recursion.java
@@ -572,26 +572,42 @@
 //     }
 // } 
 
+// import java.util.Arrays;
+
+// public class Recursion {
+//     public static void main(String[] args) {
+//         int[] a = {1, 3, 5};
+//         int[] b = {2, 4, 6};
+
+//         int[] c = new int[a.length + b.length];
+
+//         int i = 0, j = 0, k = 0;
+
+//         while (i < a.length && j < b.length) {
+//             if (a[i] < b[j]) c[k++] = a[i++];
+//             else c[k++] = b[j++];
+//         }
+
+//         while (i < a.length) c[k++] = a[i++];
+//         while (j < b.length) c[k++] = b[j++];
+
+//         System.out.println(Arrays.toString(c));
+//     }
+// }
+
+
+
 import java.util.Arrays;
-
 public class Recursion {
-    public static void main(String[] args) {
-        int[] a = {1, 3, 5};
-        int[] b = {2, 4, 6};
 
-        int[] c = new int[a.length + b.length];
+public static void main(String[] args) {
+        int[] arr = {7, 2, 5, 3, 1};
 
-        int i = 0, j = 0, k = 0;
+        int k = 3;
 
-        while (i < a.length && j < b.length) {
-            if (a[i] < b[j]) c[k++] = a[i++];
-            else c[k++] = b[j++];
-        }
+        Arrays.sort(arr);
 
-        while (i < a.length) c[k++] = a[i++];
-        while (j < b.length) c[k++] = b[j++];
-
-        System.out.println(Arrays.toString(c));
+        System.out.println("Kth smallest: " + arr[k - 1]);
     }
 }
 
