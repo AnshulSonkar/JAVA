3f41363332a35683277e085cc59ca5b120b5fe7f
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-05 23:48:43 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -354,30 +354,51 @@
 // }
 
 
+// public class Main {
+// static int binarySearch(int[] arr, int key) {
+//         int low = 0, high = arr.length - 1;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+
+//             if (arr[mid] == key) return mid;
+
+//             else if (arr[mid] < key) low = mid + 1;
+
+//             else high = mid - 1;
+//         }
+
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5, 6};
+
+//         System.out.println(binarySearch(arr, 4));
+//     }
+// }
+
+
 public class Main {
-static int binarySearch(int[] arr, int key) {
-        int low = 0, high = arr.length - 1;
+static int binarySearch(int[] arr, int low, int high, int key) {
+        if (low > high) return -1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int mid = (low + high) / 2;
 
-            if (arr[mid] == key) return mid;
+        if (arr[mid] == key) return mid;
 
-            else if (arr[mid] < key) low = mid + 1;
-
-            else high = mid - 1;
-        }
-
-        return -1;
+        if (arr[mid] < key)
+            return binarySearch(arr, mid + 1, high, key);
+        else
+            return binarySearch(arr, low, mid - 1, key);
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1,2,3,4,5};
 
-        System.out.println(binarySearch(arr, 4));
+        System.out.println(binarySearch(arr, 0, arr.length - 1, 5));
     }
 }
-
 
 
 
