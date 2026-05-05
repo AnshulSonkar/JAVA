80427171683836bc3b929b70fdbbf7b152fc7ac8
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-05 23:45:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -336,22 +336,50 @@
 // } 
 
 
+// public class Main {
+// static int linearSearch(int[] arr, int key) {
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] == key) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {4, 2, 7, 1, 9};
+
+//         System.out.println(linearSearch(arr, 7));
+//     }
+// }
+
+
 public class Main {
-static int linearSearch(int[] arr, int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key) {
-                return i;
-            }
+static int binarySearch(int[] arr, int key) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) return mid;
+
+            else if (arr[mid] < key) low = mid + 1;
+
+            else high = mid - 1;
         }
+
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 2, 7, 1, 9};
+        int[] arr = {1, 2, 3, 4, 5, 6};
 
-        System.out.println(linearSearch(arr, 7));
+        System.out.println(binarySearch(arr, 4));
     }
 }
+
+
+
 
 
 
