8c2a155c8a93ef5c0129e2e1e5afbdb5205f462f
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-05 23:57:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -436,16 +436,35 @@
 // }
 
 
+// public class Main {
+//  public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 2, 4, 2};
+//         int key = 2;
+
+//         int index = -1;
+
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] == key) {
+//                 index = i;   
+//             }
+//         }
+
+//         System.out.println(index);
+//     }
+// }
+
+
 public class Main {
- public static void main(String[] args) {
+public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 4, 2};
         int key = 2;
 
         int index = -1;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             if (arr[i] == key) {
-                index = i;   
+                index = i;
+                break;
             }
         }
 
