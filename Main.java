36f203c6a66a4294bc1048323adabacddddfc0ac
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-06 00:00:24 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -454,24 +454,40 @@
 // }
 
 
+// public class Main {
+// public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 2, 4, 2};
+//         int key = 2;
+
+//         int index = -1;
+
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             if (arr[i] == key) {
+//                 index = i;
+//                 break;
+//             }
+//         }
+
+//         System.out.println(index);
+//     }
+// }
+
+
 public class Main {
 public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 2, 4, 2};
-        int key = 2;
+        int[] arr = {5, 2, 8, 1, 4};
 
-        int index = -1;
+        int min = arr[0];
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] == key) {
-                index = i;
-                break;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
 
-        System.out.println(index);
+        System.out.println("Smallest: " + min);
     }
 }
-
 
 
 
