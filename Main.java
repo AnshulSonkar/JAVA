5c45cdce1c2e3efcf7ce9a40c4e83c3ccbd41872
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-06 23:45:58 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -507,14 +507,29 @@
 // }
 
 
-import java.util.Arrays;
-public class Main {
-public static void main(String[] args) {
+// import java.util.Arrays;
+// public class Main {
+// public static void main(String[] args) {
+//         int[] arr = {5, 2, 8, 1, 4};
+
+//         Arrays.sort(arr);
+
+//         System.out.println(arr[0]);
+//     }
+// } 
+
+
+ public class Main {
+static int min(int[] arr, int i) {
+        if (i == arr.length - 1) return arr[i];
+
+        return Math.min(arr[i], min(arr, i + 1));
+    }
+
+    public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1, 4};
 
-        Arrays.sort(arr);
-
-        System.out.println(arr[0]);
+        System.out.println(min(arr, 0));
     }
 }
 
