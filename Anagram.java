b7b48d6bdc153bc68e4404dd2001fe7f56cddda0
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-11 22:57:21 +0530

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -71,19 +71,43 @@
 // }
 
 
-public class Anagram {
-    public static void main(String[] args) {
-        String str = "madam";
-        String rev = "";
+// public class Anagram {
+//     public static void main(String[] args) {
+//         String str = "madam";
+//         String rev = "";
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev += str.charAt(i);
-        }
+//         for (int i = str.length() - 1; i >= 0; i--) {
+//             rev += str.charAt(i);
+//         }
 
-        if (str.equals(rev)) {
-            System.out.println("Palindrome");
+//         if (str.equals(rev)) {
+//             System.out.println("Palindrome");
+//         } else {
+//             System.out.println("Not Palindrome");
+//         }
+//     }
+// }
+
+
+import java.util.Arrays;
+ public class Anagram {
+  public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("Anagram");
         } else {
-            System.out.println("Not Palindrome");
+            System.out.println("Not Anagram");
         }
     }
 }
+
+
