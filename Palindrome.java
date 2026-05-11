406016bf142e34d9dc25535416236da383f8d32f
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-11 22:53:14 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,18 +1,44 @@
+// public class Palindrome {
+//     public static void main(String[] args) {
+
+//         String str = "madam";
+
+//         String rev = "";
+
+//         for (int i = str.length() - 1; i >= 0; i--) {
+//             rev += str.charAt(i);
+//         }
+
+//         if (str.equals(rev)) {
+//             System.out.println("Palindrome");
+//         } else {
+//             System.out.println("Not Palindrome");
+//         }
+//     }
+// }
+
+
 public class Palindrome {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
 
-        String str = "madam";
+        String str = "level";
 
-        String rev = "";
+        int left = 0;
+        int right = str.length() - 1;
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev += str.charAt(i);
+        boolean isPalindrome = true;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
         }
 
-        if (str.equals(rev)) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
-        }
+        System.out.println(isPalindrome ? "Palindrome" : "Not Palindrome");
     }
 }
