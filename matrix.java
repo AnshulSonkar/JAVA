ce0c759b458165be75aab1457101afacc75e5ed1
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-12 22:53:16 +0530

    Add files via upload

--- matrix.java
+++ matrix.java
@@ -51,25 +51,50 @@
 
 
 
+// public class matrix {
+//  public static void main(String[] args) {
+
+//         int[][] arr = {
+//             {1, 2},
+//             {3, 4}
+//         };
+
+//         int sum = 0;
+
+//         for (int i = 0; i < arr.length; i++) {
+
+//             for (int j = 0; j < arr[i].length; j++) {
+
+//                 sum += arr[i][j];
+//             }
+//         }
+
+//         System.out.println("Sum = " + sum);
+//     }
+// }
+
+
 public class matrix {
  public static void main(String[] args) {
 
         int[][] arr = {
-            {1, 2},
-            {3, 4}
+            {1, 8, 3},
+            {4, 5, 6}
         };
 
-        int sum = 0;
+        int max = arr[0][0];
 
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = 0; j < arr[i].length; j++) {
 
-                sum += arr[i][j];
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
             }
         }
 
-        System.out.println("Sum = " + sum);
+        System.out.println("Largest = " + max);
     }
 }
 
