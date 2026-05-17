f9f0d2bd0df49e36899b559c8e84cbf85b780b7b
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-17 21:16:30 +0530

    Add files via upload

--- oop.java
+++ oop.java
@@ -22,27 +22,56 @@
 // }
 
 
-class Student {
+// Constructor
 
-    String name;
-    int age;
+// class Student {
 
-    Student(String n, int a) {
-        name = n;
-        age = a;
+//     String name;
+//     int age;
+
+//     Student(String n, int a) {
+//         name = n;
+//         age = a;
+//     }
+
+//     void display() {
+//         System.out.println(name + " " + age);
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+
+//         Student s1 = new Student("Aman", 20);
+
+//         s1.display();
+//     }
+// }
+
+
+// Inheritance
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
     }
+}
 
-    void display() {
-        System.out.println(name + " " + age);
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog barks");
     }
 }
 
 public class oop {
     public static void main(String[] args) {
 
-        Student s1 = new Student("Aman", 20);
+        Dog d = new Dog();
 
-        s1.display();
+        d.sound();
+        d.bark();
     }
 }
    
