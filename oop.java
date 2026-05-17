889ddd11a9b7d0dc27b72a79210071d4ca066803
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-17 21:12:13 +0530

    Add files via upload

--- oop.java
+++ oop.java
@@ -1,21 +1,46 @@
+// class Student {
+
+//     String name;
+//     int age;
+
+//     void display() {
+//         System.out.println(name);
+//         System.out.println(age);
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+
+//         Student s1 = new Student();
+
+//         s1.name = "Anshul";
+//         s1.age = 19;
+
+//         s1.display();
+//     }
+// }
+
+
 class Student {
 
     String name;
     int age;
 
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
     void display() {
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(name + " " + age);
     }
 }
 
 public class oop {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-
-        s1.name = "Anshul";
-        s1.age = 19;
+        Student s1 = new Student("Aman", 20);
 
         s1.display();
     }
