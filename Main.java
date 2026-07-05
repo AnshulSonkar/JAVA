5fcc7fe5a540153d12ea5caec99d61e416ba2ff5
AnshulSonkar <amshulsonkar2222@gmail.com>
2026-07-05 21:18:43 +0530

    commit

--- Main.java
+++ Main.java
@@ -1708,3 +1708,58 @@
 //         );
 //     }
 // }
+
+
+// Space Optimized
+// public class Main {
+
+//     static int minPathSum(int[][] grid){
+
+//         int m=grid.length;
+//         int n=grid[0].length;
+
+//         int[] prev=new int[n];
+
+//         for(int i=0;i<m;i++){
+
+//             int[] curr=new int[n];
+
+//             for(int j=0;j<n;j++){
+
+//                 if(i==0 && j==0){
+
+//                     curr[j]=grid[0][0];
+//                     continue;
+//                 }
+
+//                 int up=Integer.MAX_VALUE;
+//                 int left=Integer.MAX_VALUE;
+
+//                 if(i>0)
+//                     up=prev[j];
+
+//                 if(j>0)
+//                     left=curr[j-1];
+
+//                 curr[j]=grid[i][j]+Math.min(up,left);
+//             }
+
+//             prev=curr;
+//         }
+
+//         return prev[n-1];
+//     }
+
+//     public static void main(String[] args){
+
+//         int[][] grid={
+//                 {1,3,1},
+//                 {1,5,1},
+//                 {4,2,1}
+//         };
+
+//         System.out.println(
+//                 minPathSum(grid)
+//         );
+//     }
+// }
