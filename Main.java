50faf7ae26e0fe5d9290c266e4ed1b55252ed0cd
AnshulSonkar <amshulsonkar2222@gmail.com>
2026-07-05 21:21:50 +0530

    commit

--- Main.java
+++ Main.java
@@ -1763,3 +1763,41 @@
 //         );
 //     }
 // }
+
+
+// Triangle
+// import java.util.*;
+
+// public class Main {
+
+//     static int solve(List<List<Integer>> triangle, int i, int j){
+
+//         if(i == triangle.size()-1)
+//             return triangle.get(i).get(j);
+
+//         int down =
+//                 solve(triangle,i+1,j);
+
+//         int diagonal =
+//                 solve(triangle,i+1,j+1);
+
+//         return triangle.get(i).get(j)
+//                 + Math.min(down,diagonal);
+//     }
+
+//     public static void main(String[] args){
+
+//         List<List<Integer>> triangle=new ArrayList<>();
+
+//         triangle.add(Arrays.asList(2));
+//         triangle.add(Arrays.asList(3,4));
+//         triangle.add(Arrays.asList(6,5,7));
+//         triangle.add(Arrays.asList(4,1,8,3));
+
+//         System.out.println(
+//                 solve(triangle,0,0)
+//         );
+//     }
+// }
+
+
