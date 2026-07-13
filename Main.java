7d81ba62e73dbb57b8446da7412becb242d313e9
AnshulSonkar <amshulsonkar2222@gmail.com>
2026-07-13 21:23:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4715,6 +4715,58 @@
 //         );
 //     }
 // }
+// 
+// 
+// Burst Balloons - Memoization
+// import java.util.Arrays;
+
+// public class Main {
+
+//     static int solve(int[] arr, int i, int j, int[][] dp) {
+
+//         if (i > j)
+//             return 0;
+
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+
+//         int ans = 0;
+
+//         for (int k = i; k <= j; k++) {
+
+//             int coins =
+//                     solve(arr, i, k - 1, dp)
+//                     + solve(arr, k + 1, j, dp)
+//                     + arr[i - 1] * arr[k] * arr[j + 1];
+
+//             ans = Math.max(ans, coins);
+//         }
+
+//         return dp[i][j] = ans;
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] nums = {3,1,5,8};
+
+//         int[] arr = new int[nums.length + 2];
+
+//         arr[0] = 1;
+//         arr[arr.length - 1] = 1;
+
+//         for (int i = 0; i < nums.length; i++)
+//             arr[i + 1] = nums[i];
+
+//         int[][] dp = new int[arr.length][arr.length];
+
+//         for (int[] row : dp)
+//             Arrays.fill(row, -1);
+
+//         System.out.println(
+//                 solve(arr,1,nums.length,dp)
+//         );
+//     }
+// }
 
 
 
