8fff6da2a46babfd6be4e13ff5c20697122eb8dd
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-07-14 20:37:19 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -4933,6 +4933,79 @@
 // }
 // 
 // 
+// Scrambling String (Recursion + Memoization)
+// import java.util.HashMap;
+
+// public class Main {
+
+//     static HashMap<String, Boolean> memo = new HashMap<>();
+
+//     static boolean isScramble(String s1, String s2) {
+
+//         if (s1.equals(s2))
+//             return true;
+
+//         if (s1.length() != s2.length())
+//             return false;
+
+//         String key = s1 + "#" + s2;
+
+//         if (memo.containsKey(key))
+//             return memo.get(key);
+
+//         int[] freq = new int[26];
+
+//         for (int i = 0; i < s1.length(); i++) {
+//             freq[s1.charAt(i) - 'a']++;
+//             freq[s2.charAt(i) - 'a']--;
+//         }
+
+//         for (int x : freq) {
+//             if (x != 0) {
+//                 memo.put(key, false);
+//                 return false;
+//             }
+//         }
+
+//         int n = s1.length();
+
+//         for (int k = 1; k < n; k++) {
+
+//             // No Swap
+//             if (isScramble(s1.substring(0, k),
+//                            s2.substring(0, k))
+//                     &&
+//                 isScramble(s1.substring(k),
+//                            s2.substring(k))) {
+
+//                 memo.put(key, true);
+//                 return true;
+//             }
+
+//             // Swap
+//             if (isScramble(s1.substring(0, k),
+//                            s2.substring(n - k))
+//                     &&
+//                 isScramble(s1.substring(k),
+//                            s2.substring(0, n - k))) {
+
+//                 memo.put(key, true);
+//                 return true;
+//             }
+//         }
+
+//         memo.put(key, false);
+//         return false;
+//     }
+
+//     public static void main(String[] args) {
+
+//         String s1 = "great";
+//         String s2 = "rgeat";
+
+//         System.out.println(isScramble(s1, s2));
+//     }
+// }
 
 
 
